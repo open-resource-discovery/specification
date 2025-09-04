65b11b1033a6344806e2c9c7a1c3e8115565e481
pavelkornev <pavelkornev@gmail.com>
2025-09-04 17:44:05 +0000

    Deploying to gh-pages from @ open-resource-discovery/specification@7949965501cd2967092e9e0cab8d84769ba00c58 🚀

--- js/custom.js
+++ js/custom.js
@@ -24,3 +24,28 @@ function addAnchorTitle(anchorTitle) {
   anchorTitle = anchorTitle.replace("Direct link to ", "");
   document.title = `${anchorTitle} | ${split[split.length - 1]} | ${split[split.length - 2]}`;
 }
+
+var __NAV_SCROLL_THRESHOLD__ = 2;
+var __navScrollTicking__ = false;
+
+function __applyNavScrolled__() {
+  var scrolled = (window.scrollY || window.pageYOffset) > __NAV_SCROLL_THRESHOLD__;
+  var val = scrolled ? "1" : "0";
+  var html = document.documentElement;
+  if (html.getAttribute("data-nav-scrolled") !== val) {
+    html.setAttribute("data-nav-scrolled", val);
+  }
+  __navScrollTicking__ = false;
+}
+
+function __onNavScroll__() {
+  if (!__navScrollTicking__) {
+    __navScrollTicking__ = true;
+    window.requestAnimationFrame(__applyNavScrolled__);
+  }
+}
+
+window.addEventListener("load", __applyNavScrolled__, false);
+window.addEventListener("scroll", __onNavScroll__, { passive: true });
+window.addEventListener("resize", __onNavScroll__);
+window.addEventListener("orientationchange", __onNavScroll__);
