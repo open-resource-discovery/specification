18927ef7ae75a9426bb3f5b271fd717af9176307
Simon Heimler <heimlersimon@gmail.com>
2026-02-04 20:46:31 +0100

    Adding linting via biome and fixing lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import * as staticDocumentSchema from "./generated/spec/v1/schemas/Document.schema.json";
+import type { SpecJsonSchemaRoot } from "@open-resource-discovery/spec-toolkit";
 import * as staticConfigurationSchema from "./generated/spec/v1/schemas/Configuration.schema.json";
-import { SpecJsonSchemaRoot } from "@open-resource-discovery/spec-toolkit";
+import * as staticDocumentSchema from "./generated/spec/v1/schemas/Document.schema.json";
 
 export * from "./generated/spec/v1/types";
 
 /**
  * The JSON Schema (draft-07) describing the ORD Document interface
  */
-export const ordDocumentSchema = staticDocumentSchema as unknown as SpecJsonSchemaRoot;
+export const ordDocumentSchema =
+	staticDocumentSchema as unknown as SpecJsonSchemaRoot;
 
 /**
  * The JSON Schema (draft-07) describing the ORD Configuration interface
  */
-export const ordConfigurationSchema = staticConfigurationSchema as unknown as SpecJsonSchemaRoot;
+export const ordConfigurationSchema =
+	staticConfigurationSchema as unknown as SpecJsonSchemaRoot;
