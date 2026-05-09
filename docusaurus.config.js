daf1eedda202b71fbbd574303139e4812adc9f47
Simon Heimler <simon.heimler@sap.com>
2026-05-09 15:51:48 +0200

    Update to latest docusaurus and enable faster mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,6 +18,13 @@ const config = {
 	staticDirectories: ["static"],
 	favicon: "img/favicon.svg",
 
+	future: {
+		faster: true,
+		v4: {
+			removeLegacyPostBuildHeadAttribute: true,
+		},
+	},
+
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
 	// to replace "en" with "zh-Hans".
