69d496c37f32f3871b7b6a968654b6cddb400d62
Simon Heimler <simon.heimler@sap.com>
2026-09-13 11:23:57 +0200

    chore: modernize Node toolchain
    Signed-off-by: Simon Heimler <simon.heimler@sap.com>

--- src/helper/log.ts
+++ src/helper/log.ts
@@ -1,10 +1,5 @@
-import { Logger } from "tslog";
-
 /**
  * Logging instance and settings that are shared by all
  * scripts and CLIs in this repo
  */
-export const log = new Logger({
-  type: "pretty",
-  prettyLogTemplate: "{{hh}}:{{MM}}:{{ss}} {{logLevelName}} ",
-});
+export const log = console;
